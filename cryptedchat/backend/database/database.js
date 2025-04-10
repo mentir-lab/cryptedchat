d25f02d8189e7d4f264e753f0ebdd63da143c8f2
mentir-lab <vip.dron.2004@mail.ru>
2025-04-10 08:30:01 +0800

    third commit

--- cryptedchat/backend/database/database.js
+++ cryptedchat/backend/database/database.js
@@ -1,9 +1,22 @@
 var admin = require("firebase-admin");
 require('dotenv').config();
-var serviceAccount = require("./serviceAccountKeys.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(
+        {
+            "type": process.env.TYPE,
+            "project_id": process.env.ID,
+            "private_key_id": process.env.PRIVATE_KEY_ID,
+            "private_key": process.env.PRIVATE_KEY,
+            "client_email": process.env.EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": process.env.AUTH,
+            "token_uri": process.env.TOKEN,
+            "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
+            "client_x509_cert_url": process.env.CLIENT_URL,
+            "universe_domain": process.env.DOMAIN
+        }
+    ),
     databaseURL: "https://cryptedbase-36c89-default-rtdb.europe-west1.firebasedatabase.app"
 });
 module.exports = { admin }
